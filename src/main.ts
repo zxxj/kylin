bbf80497c5de8c91a813d4f37b0f32744286d47d
zhangxinxin <1285367184@qq.com>
2024-08-05 22:54:21 +0800

    ♻️chore: 引入antdvue4.x并删除脚手架无用文件

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/reset.css'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
+app.use(Antd)
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
